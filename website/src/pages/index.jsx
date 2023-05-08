34c89cbaa373307f2ac518e5a042969660110466
Merill Fernando <merill@merill.net>
2023-05-08 11:10:39 +1000

    Update index.jsx

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -18,7 +18,7 @@ export default function Start() {
           <meta property="og:url" content="https://aka.cmd.ms" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Search aka.ms" />
-          <meta property="og:description" content="Search for aka.ms links" />
+          <meta property="og:description" content="Use this page to search for aka.ms links that have been contributed to by the community." />
           <meta property="og:image" content="https://aka.cmd.ms/OpenGraphImage.png" />
           <meta property="og:image:alt" content="Screenshot of aka.cmd.ms home page with the text 'Looking for aka.ms links? Find them at aka.cmd.ms'" />
 
@@ -27,7 +27,7 @@ export default function Start() {
           <meta property="twitter:site" content="@merill" />
           <meta property="twitter:url" content="https://aka.cmd.ms" />
           <meta name="twitter:title" content="Search aka.ms" />
-          <meta name="twitter:description" content="Search for aka.ms links." />
+          <meta name="twitter:description" content="Use this page to search for aka.ms links that have been contributed to by the community." />
           <meta name="twitter:image" content="https://aka.cmd.ms/OpenGraphImage.png" />
           <meta property="twitter:image:alt" content="Screenshot of aka.cmd.ms home page with the text 'Looking for aka.ms links? Find them at aka.cmd.ms'" />
         </Head>
